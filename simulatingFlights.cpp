b058e11ce622d9f0234ce187e23519dad0683cf1
Marcos França <86272462+MrcsFranca@users.noreply.github.com>
2025-05-13 08:25:43 -0300

    Update and rename simulaVoo.cpp to simulatingFlights.cpp
    - I changed the name of the file
    - For each time unit, 5 flights arrive

--- simulatingFlights.cpp
+++ simulatingFlights.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <unistd.h>
-//#include "FilaPrioridadesLE.h"
+#define numAviao 50
 
 using namespace std;
 
@@ -19,12 +19,12 @@ void exibe(no *L);
 int main() {
     system("clear");
     srand(time(NULL));
-    no *FP1, *FP2;
-    int x = 0, prior = 0;
+    int x = 0, prior = 0, tempoEstimado = 0;
+    no *FP1, *FP2, *P1, *P2;
 
     FP1 = FP2 = NULL;
 
-    for(int i = 1; i <= 10; i++) {
+    for(int i = 1; i <= numAviao; i++) {
         FP1 = insere(FP1, i, (rand()%2)+1);
         FP2 = insere(FP2, i, (rand()%2)+1);
     }
@@ -42,8 +42,7 @@ int main() {
     sleep(10);
     system("clear");
 
-    no *P1, *P2;
-    for(int ut = 1; (vazia(FP1) == 0) || (vazia(FP2) == 0); ut++) {
+    for(int ut = 1, numImpresso = 5; (vazia(FP1) == 0) || (vazia(FP2) == 0); ut++, numImpresso+=5) {
         cout << endl << "Unidade de tempo: " << ut  << endl;
         cout << endl << "------------------------------------------------------------------" << endl;
 
@@ -52,8 +51,9 @@ int main() {
             cout << "Pista de pouso atendendo o aviao: " << P1->info << " | P: " << P1->prior << endl;
             cout << endl << endl << "=====Avioes que desejam pousar=====" << endl;
             P1 = P1->link;
-            for(int i = 1; (i < 5) && P1 != NULL; i++) {
-                cout << "A: " << P1->info << " | P: " << P1->prior << endl;
+            for(int i = 1;(i < numImpresso) && (P1 != NULL); i++) {
+                tempoEstimado = i * 2;
+                cout << "A: " << P1->info << " | P: " << P1->prior << " | T.A. de espera: " << tempoEstimado << endl;
                 P1 = P1->link;
             }
             if(P1 == NULL) {
@@ -76,8 +76,9 @@ int main() {
             cout << "Pista de decolagem atendendo o aviao: " << P2->info << " | P: " << P2->prior << endl;
             cout << endl << endl << "=====Avioes que desejam decolar=====" << endl;
             P2 = P2->link;
-            for(int i = 1; (i < 5) && P2 != NULL; i++) {
-                cout << "A: " << P2->info << " | P: " << P2->prior << endl;
+            for(int i = 1; (i < numImpresso) && P2 != NULL; i++) {
+                tempoEstimado = i * 2;
+                cout << "A: " << P2->info << " | P: " << P2->prior << " | T.A. de espera: " << tempoEstimado << endl;
                 P2 = P2->link;
             }
             if(P2 == NULL) {
@@ -96,6 +97,8 @@ int main() {
         sleep(5);
         system("clear");
     }
+
+    cout << endl << "A simulação terminou... obrigado por usar" << endl;
 
     return 0;
 }
@@ -160,5 +163,3 @@ void exibe(no *L) {
         P = P->link;
     }
 }
-
-
